c9247a41192b9b6d70f961a1a1a8291a72fac68f
Vaibhav Garg <gargvaibav@gmail.com>
2018-07-20 15:21:48 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,18 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	
+    	while(1){
+    		int temp;
+    		scanf("%d", &temp);
+    		if(temp!=42){
+    			printf("%d\n", temp);
+    		}
+    		else{
+    			break;
+    		}
+    	}
+     
+    	return 0;
+    } 
