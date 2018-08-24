6924bd4b912e52cf06bc8b12b82577d46ba038b0
Vaibhav Garg <gargvaibav@gmail.com>
2018-08-24 20:32:25 +0530

    Create BLLUCK.cpp

---
+++ BLLUCK.cpp
@@ -0,0 +1,16 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main(){
+    	int T;
+    	scanf("%d", &T);
+    	while(T--){
+    		int n;
+    		scanf("%d", &n);
+    		if(n==0){
+    			printf("0.000000\n");
+    		}
+    		else{
+    			printf("1.000000\n");
+    		}
+    	}
+    } 
